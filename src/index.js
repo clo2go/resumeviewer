a78a4504f7eed9ca37f4e1f4f77589caf1a06b54
clo2go@gmail.com <clo2go@gmail.com>
2016-12-12 15:33:43 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var ReactDOM = require('./App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
