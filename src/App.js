f4c5db39cf122814cfa1b439b231fdadbecfa3bc
clo2go@gmail.com <clo2go@gmail.com>
2016-12-13 10:15:28 -0600

    npm runbuild, installed all requeired dependencies

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'http://localhost:3000/resumeData.json',
+      url:'https://localhost:3000/resumeData.json',
       dataType: 'json',
       cache: false,
       success: function(data){
