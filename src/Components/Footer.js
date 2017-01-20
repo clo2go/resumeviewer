0e510092c1c2125120d8517323a2645dfa09b37b
clo2go <clo2go@gmail.com>
2017-01-19 22:26:52 -0600

    footer adjustments

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,8 +16,7 @@ class Footer extends Component {
                <li><a href="#"><i className="fa fa-skype"></i></a></li>
             </ul>
             <ul className="copyright">
-               <li>&copy; Copyright 2014 Clo2goCo</li>
-               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>   
+               <li>&copy; Copyright 2017 Clo2goCo</li>   
             </ul>
          </div>
          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
